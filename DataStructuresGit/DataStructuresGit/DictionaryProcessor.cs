9b6cc1aef19a04b9d9fca60762a2d9412689a246
jjs254 <32311508+jjs254@users.noreply.github.com>
2017-09-28 20:10:14 -0600

    Dictionary class updated

--- DataStructuresGit/DataStructuresGit/DictionaryProcessor.cs
+++ DataStructuresGit/DataStructuresGit/DictionaryProcessor.cs
@@ -6,24 +6,30 @@ using System.Threading.Tasks;
 
 namespace DataStructuresGit
 {
+    //Processes all operations related to a dictionary
     class DictionaryProcessor
     {
         private Dictionary<string, int> myDictionary;
         private int valueCounter;
         private System.Diagnostics.Stopwatch sw; 
 
+        //Constructor
         private DictionaryProcessor()
         {
             myDictionary = new Dictionary<string, int>();
             valueCounter = 1;
             sw = new System.Diagnostics.Stopwatch();
         }
+
+        //Adds one item to the dictionary
         public void addOneItem(string newEntry)
         {
             myDictionary.Add(newEntry, valueCounter);
             valueCounter = valueCounter + 1;
+            Console.WriteLine("newEntry" + " was added to the dictionary");
         }
 
+        //Adds 2000 items to the dictionary
         public void addHugeList()
         {
             clear();
@@ -33,15 +39,17 @@ namespace DataStructuresGit
                 myDictionary.Add(valueToEnter, valueCounter);
                 valueCounter++;
             }
+            Console.WriteLine("2000 items added to the dictionary");
         }
 
+        //Displays all items in the dictionary
         public void display()
         {
-            if (myDictionary.Count == 0)
+            if (myDictionary.Count == 0) //if nothing is in the dictionary
             {
-                Console.WriteLine("Nothing is in the dictionary.");
+                Console.WriteLine("There is nothing to display");
             }
-            else
+            else //stuff is in the dictionary
             {
                 foreach (KeyValuePair<string, int> pair in myDictionary)
                 {
@@ -52,38 +60,48 @@ namespace DataStructuresGit
             }
         }
 
+        //deletes a specified item from the dictionary
         public void delete(string item)
         {
-            if(myDictionary.Count == 0)
+            if(myDictionary.Count == 0) //if nothing is in the dictionary
             {
                 Console.WriteLine("Nothing is in the dictionary.");
             }
-            else if (myDictionary.ContainsKey(item))
+            else if (myDictionary.ContainsKey(item)) //if the thing is in the dictionary
             {
                 myDictionary.Remove(item);
                 valueCounter--;
+                Console.WriteLine("Deleted " + item);
             }
-            else
+            else //the thing is not in the dictionary
             {
                 Console.WriteLine("That element was not in the dictionary to begin with.");
             }
         }
 
+        //clears the dictionary
         public void clear()
         {
             valueCounter = 1;
             myDictionary.Clear();
+            Console.WriteLine("Dictionary cleared");
         }
 
+        //searches the dictionary for a specified string; returns whether it was found and how long it took
         public void search(string item)
         {
             sw.Start();
-            if (myDictionary.ContainsKey(item))
+            if (myDictionary.ContainsKey(item)) //item is in the dictionary
             {
                 int foundValue = myDictionary[item];
                 sw.Stop();
                 Console.WriteLine("Found!!  " + "Time: " + sw.Elapsed);
-            }   
+            }
+            else //item is not in the dictionary
+            {
+                sw.Stop();
+                Console.WriteLine("Not found!!" + "Time: " + sw.Elapsed);
+            }
         }
     }
 }
