cb87216cc4293b40d98189cb4da08e6db085b8c1
Bryce LeFevre <brycelefevre>
2017-09-28 19:07:40 -0600

    none

--- DataStructuresGit/DataStructuresGit/Program.cs
+++ DataStructuresGit/DataStructuresGit/Program.cs
@@ -9,13 +9,7 @@ namespace DataStructuresGit
     class Program
     {
         static void Main(string[] args)
-        {
-            //menue
-            Console.WriteLine("Stack\nQueue\nDictionary\nExit");
-
-            //pauses the program so user can view screen
-            Console.Read();
-            
+        {          
 
         }
     }
