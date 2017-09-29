83df40a2ba2c749679b77d7911c9f046598022ac
jjs254 <32311508+jjs254@users.noreply.github.com>
2017-09-28 19:41:07 -0600

    Dictionary class finished

--- DataStructuresGit/DataStructuresGit/DictionaryProcessor.cs
+++ DataStructuresGit/DataStructuresGit/DictionaryProcessor.cs
@@ -9,35 +9,81 @@ namespace DataStructuresGit
     class DictionaryProcessor
     {
         private Dictionary<string, int> myDictionary;
+        private int valueCounter;
+        private System.Diagnostics.Stopwatch sw; 
 
+        private DictionaryProcessor()
+        {
+            myDictionary = new Dictionary<string, int>();
+            valueCounter = 1;
+            sw = new System.Diagnostics.Stopwatch();
+        }
         public void addOneItem(string newEntry)
         {
-
+            myDictionary.Add(newEntry, valueCounter);
+            valueCounter = valueCounter + 1;
         }
 
         public void addHugeList()
         {
-
+            clear();
+            for (int counter = 0; counter < 2000; counter++)
+            {
+                string valueToEnter = "New Entry " + valueCounter;
+                myDictionary.Add(valueToEnter, valueCounter);
+                valueCounter++;
+            }
         }
 
         public void display()
         {
-
+            if (myDictionary.Count == 0)
+            {
+                Console.WriteLine("Nothing is in the dictionary.");
+            }
+            else
+            {
+                foreach (KeyValuePair<string, int> pair in myDictionary)
+                {
+                    String key = pair.Key;
+                    int value = pair.Value;
+                    Console.WriteLine(key + " " + value);
+                }
+            }
         }
 
         public void delete(string item)
         {
-
+            if(myDictionary.Count == 0)
+            {
+                Console.WriteLine("Nothing is in the dictionary.");
+            }
+            else if (myDictionary.ContainsKey(item))
+            {
+                myDictionary.Remove(item);
+                valueCounter--;
+            }
+            else
+            {
+                Console.WriteLine("That element was not in the dictionary to begin with.");
+            }
         }
 
         public void clear()
         {
-
+            valueCounter = 1;
+            myDictionary.Clear();
         }
 
-        public string search(string item)
+        public void search(string item)
         {
-            return null;
+            sw.Start();
+            if (myDictionary.ContainsKey(item))
+            {
+                int foundValue = myDictionary[item];
+                sw.Stop();
+                Console.WriteLine("Found!!  " + "Time: " + sw.Elapsed);
+            }   
         }
     }
 }
